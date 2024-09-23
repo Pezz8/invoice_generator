63a792ffa8fce68c97320ebefefefee72011db51
Nick Boyadjian <nick.boyadjian@podium.com>
2024-09-23 19:18:11 -0400

    refactor: move template generation to new function

--- index.js
+++ index.js
@@ -28,22 +28,40 @@ function readTemplate(templatePath) {
   return fs.readFileSync(templatePath, "utf-8");
 }
 
+function getInvoiceAndPath(type, unitNumber, invoiceNumber, templates) {
+  if (type.toUpperCase() == "K") {
+    return {
+      template: templates.kTemplate,
+      pdfPath: `${__dirname}/invoices/Regatta ${unitNumber} Key Order Invoice ${invoiceNumber}.pdf`,
+    };
+  } else if (type.toUpperCase() == "F") {
+    return {
+      template: templates.fTemplate,
+      pdfPath: `${__dirname}/invoices/Regatta ${unitNumber} HVAC Filter Invoice ${invoiceNumber}.pdf`,
+    };
+  } else {
+    return {
+      template: templates.woTemplate,
+      pdfPath: `${__dirname}/invoices/Regatta ${unitNumber} Work Order Invoice ${invoiceNumber}.pdf`,
+    };
+  }
+}
+
 readXlsxFile(path, { sheet: "July 24", dateFormat: "MM-DD-YYYY" }).then(
   async (rows) => {
-    const results = rows.filter((row) => row[0] != null).toSpliced(0, 2);
+    const results = rows.filter((row) => row[0] != null)?.toSpliced(0, 2);
 
     // Read the HTML template from file
     const woTemplate = readTemplate(woTemplatePath);
     const kTemplate = readTemplate(kTemplatePath);
     const fTemplate = readTemplate(fTemplatePath);
+    const templates = { woTemplate, kTemplate, fTemplate };
 
     // Launch Puppeteer once for all invoices
     const browser = await puppeteer.launch();
 
     for (const row of results) {
       //const formattedToday = formatDate(today);
-      let invoiceHTML = "";
-      let pdfPath = "";
       const formattedToday = moment(today).format("MMMM Do YYYY");
       const unitNumber = row[0];
       let date = row[1];
@@ -60,41 +78,26 @@ readXlsxFile(path, { sheet: "July 24", dateFormat: "MM-DD-YYYY" }).then(
 
       const totalAmount = parts + labor;
 
-      // Select correct template and correct path based on the type of charge
-      if (type.toUpperCase() == "K") {
-        invoiceHTML = replaceTemplatePlaceholders(kTemplate, {
-          formattedToday,
-          unitNumber,
-          date,
-          invoiceNumber,
-          totalAmount,
-        });
-        pdfPath = `${__dirname}/invoices/Regatta ${unitNumber} Key Order Invoice ${invoiceNumber}.pdf`;
-      } else if (type.toUpperCase() == "F") {
-        invoiceHTML = replaceTemplatePlaceholders(fTemplate, {
-          formattedToday,
-          unitNumber,
-          date,
-          invoiceNumber,
-          totalAmount,
-        });
-        pdfPath = `${__dirname}/invoices/Regatta ${unitNumber} HVAC Filter Invoice ${invoiceNumber}.pdf`;
-      } else {
-        invoiceHTML = replaceTemplatePlaceholders(woTemplate, {
-          formattedToday,
-          unitNumber,
-          date,
-          invoiceNumber,
-          totalAmount,
-        });
-        pdfPath = `${__dirname}/invoices/Regatta ${unitNumber} Work Order Invoice ${invoiceNumber}.pdf`;
-      }
+      const { template, pdfPath } = getInvoiceAndPath(
+        type,
+        unitNumber,
+        invoiceNumber,
+        templates,
+      );
+
+      const invoiceHTML = replaceTemplatePlaceholders(template, {
+        formattedToday,
+        unitNumber,
+        date,
+        invoiceNumber,
+        totalAmount,
+      });
 
       // Check if the file already exists, and skip if it does
       if (fileExists(pdfPath)) {
-        console.log(
-          `Invoice for Unit ${unitNumber} already exists. Skipping...`
-        );
+        // console.log(
+        //   `Invoice for Unit ${unitNumber} already exists. Skipping...`,
+        // );
         continue; // Skip to the next iteration if the file exists
       }
 
@@ -117,5 +120,5 @@ readXlsxFile(path, { sheet: "July 24", dateFormat: "MM-DD-YYYY" }).then(
 
     // Close Puppeteer browser after all PDFs are generated
     await browser.close();
-  }
+  },
 );
