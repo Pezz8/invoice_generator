12ad4753136223fd4cd1644811e10f5c0e22ba0a
Cristian Guarino <43047594+Pezz8@users.noreply.github.com>
2026-03-15 12:56:50 -0400

    Cleaup (#8)
    * removed all prisma related files, keeping schema for now to reference when working on setting up database
    
    * cleanup

--- src/invoices/update.js
+++
@@ -1,8 +0,0 @@
-import prisma from '../db/prismaClient.js';
-
-export async function updateInvoiceByUUID(invoiceUuid, updates) {
-  return await prisma.invoices.update({
-    where: { uuid: invoiceUuid },
-    data: updates,
-  });
-}
